04ad2d31aa7b522dd4d43d6883b3947bf210b06a
Jeamastockentreprise <theomarion08@gmail.com>
2020-02-14 12:04:55 +0100

    fix navigation

---
+++ HTML/function/navbar.php
@@ -0,0 +1,35 @@
+<nav class="navbar navbar-inverse" id="navbar">
+                <div class="container-fluid">
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <img src="../../img/logo/logo_tunis.png" alt="icon" id="navicon" height="50px">
+                    </div>
+                    <div class="collapse navbar-collapse" id="myNavbar">
+                        <ul class="nav navbar-nav">
+                            <li class=""><a href="../../home">Accueil</a></li>
+                            <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Mes Projets<span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="../../html/fr/siteweb.html">Site web</a></li>
+                        <li><a href="../../html/fr/programme.html">Programme</a></li>
+                        <li><a href="../../html/fr/jeux.html">Jeux</a></li>
+                    </ul>
+                       </li>
+                            <li><a href="../../html/fr/apropos.php">A Propos</a></li>
+                            <li><a href="../../html/fr/contact.php">Contact</a></li>
+                        </ul>
+                        <ul class="nav navbar-nav navbar-right">
+                            <li class="li-flag"><a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><img class="logolangue" src="../../img/fr.png" width="15" height="10"> Français<span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="../en/artculinaireen.html"><img class="logolangue" src="../../img/en.png" width="15" height="10"> Anglais</a></li>
+                    </ul></li>
+                        </ul>
+                    </div>
+                </div>
+</nav>
+<?php 
+ ?>
